2e19674d09f6ad1e35266659905984d2a8f6dc95
guma <vuster8@gmail.com>
2016-01-25 14:16:21 +0900

    Initial Commit

---
+++ ShopCast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShopCast
+//
+//  Created by guma on 2016. 1. 25..
+//  Copyright © 2016년 Guma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
