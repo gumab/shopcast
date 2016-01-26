385501a88269f47eb28f464eb9dd9f1f7627b7be
guma <vuster8@gmail.com>
2016-01-26 11:14:29 +0900

    swipe test with scroll view

--- ShopCast/ViewController.swift
+++ ShopCast/ViewController.swift
@@ -11,10 +11,41 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let vc0 = ViewController0(nibName: "ViewController0", bundle: nil)
+        
+        self.addChildViewController(vc0)
+        self.scrollView.addSubview(vc0.view)
+        vc0.didMoveToParentViewController(self)
+        
+        let vc1 = ViewController1(nibName: "ViewController1", bundle: nil)
+        
+        var frame1 = vc1.view.frame
+        frame1.origin.x = self.view.frame.size.width
+        vc1.view.frame = frame1
+        
+        self.addChildViewController(vc1)
+        self.scrollView.addSubview(vc1.view)
+        vc1.didMoveToParentViewController(self)
+        
+        let vc2 = ViewController2(nibName: "ViewController2", bundle: nil)
+        
+        var frame2 = vc2.view.frame
+        frame2.origin.x = self.view.frame.size.width * 2
+        vc2.view.frame = frame2
+        
+        self.addChildViewController(vc2)
+        self.scrollView.addSubview(vc2.view)
+        vc2.didMoveToParentViewController(self)
+        
+        
+        self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width*3, self.view.frame.size.height - 66)
     }
 
     override func didReceiveMemoryWarning() {
